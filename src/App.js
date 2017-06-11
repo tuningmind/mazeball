a99a183fe0c0d9107a5881f64048bd02421ec14e
tuningmind <tuningmind@gmail.com>
2017-06-10 19:03:33 -0600

    Add top level html and css

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello world.</p>
+      <div className="app">
+        <header>
+          <h2>mazeball</h2>
+        </header>
+        <main>
+          <p>Here will be a canvas with a game.</p>
+        </main>
+        <footer>
+          <p>Hello world.</p>
+        </footer>
       </div>
     );
   }
